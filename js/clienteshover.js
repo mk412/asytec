8857cbd9c395489bcdee3aa64dec71b3eeab18c3
unknown <macagomez412@gmail.com>
2017-12-16 10:50:48 -0300

    update

---
+++ js/clienteshover.js
@@ -0,0 +1,241 @@
+// hover imagenes		
+
+//pluspetrol
+c01.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-01.png");
+	}
+);
+
+c01.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-01.png");
+	}
+);
+
+
+//petrobras
+c02.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-02.png");
+	}
+);
+
+c02.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-02.png");
+	}
+);
+
+
+//NBM
+c03.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-03.png");
+	}
+);
+
+c03.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-03.png");
+	}
+);
+
+
+//walmart
+c04.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-04.png");
+	}
+);
+
+c04.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-04.png");
+	}
+);
+
+
+//petroquimica cuyo
+c05.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-05.png");
+	}
+);
+
+c05.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-05.png");
+	}
+);
+
+
+//level 3
+c06.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-06.png");
+	}
+);
+
+c06.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-06.png");
+	}
+);
+
+
+//lo jack
+c07.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-07.png");
+	}
+);
+
+c07.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-07.png");
+	}
+);
+
+
+//richet
+c08.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-08.png");
+	}
+);
+
+c08.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-08.png");
+	}
+);
+
+
+//gob buenos aires
+c09.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-09.png");
+	}
+);
+
+c09.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-09.png");
+	}
+);
+
+
+//austral
+c10.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-10.png");
+	}
+);
+
+c10.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-10.png");
+	}
+);
+
+
+
+//oldelval
+c11.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-11.png");
+	}
+);
+
+c11.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-11.png");
+	}
+);
+
+
+//skanska
+c12.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-12.png");
+	}
+);
+
+c12.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-12.png");
+	}
+);
+
+
+//toyota
+c13.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-13.png");
+	}
+);
+
+c13.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-13.png");
+	}
+);
+
+
+//ASSA
+c14.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-14.png");
+	}
+);
+
+c14.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-14.png");
+	}
+);
+
+
+//IBM
+c15.addEventListener(
+	"mouseover",
+	function(){
+		this.setAttribute("src", "imagenes/clientes/color-15.png");
+	}
+);
+
+c15.addEventListener(
+	"mouseout",
+	function(){
+		this.setAttribute("src", "imagenes/clientesbw/byn-15.png");
+	}
+);
